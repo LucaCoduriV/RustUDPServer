f9a4344284502328f1e511eb73cd48abae376893
Luca <coduri.vitor@gmail.com>
2022-11-10 23:50:55 +0100

    serialize

--- src/models/teststruct.rs
+++ src/models/teststruct.rs
@@ -1,20 +1,31 @@
 pub mod test{
-    pub struct teststruct{
-        value : i32,
-        value2 : string,
+    use std::io::Read;
+    use std::mem;
+
+    pub struct TestStruct{
+        pub value : i32,
+        pub value2 : String,
     }
 
-    impl teststruct {
-        fn new() -> Self{
-            Self{value: 10, value2: String::from("coucou")}
+    impl TestStruct {
+        pub fn new(value:i32, value2:String) -> Self{
+            Self{value, value2}
         }
 
-        fn encode(&self) -> Vec<i8>{
-            vec![]
+        pub fn encode(&self) -> Vec<u8>{
+            self.value.to_be_bytes()
+                .iter()
+                .chain(self.value2.len().to_be_bytes().iter())
+                .chain(self.value2.as_bytes().iter())
+                .map(|v| *v).collect()
         }
 
-        fn decode() -> Self{
-            new()
+        pub fn decode(data: &[u8]) -> Self{
+            let value = i32::from_be_bytes((&data[0..4]).try_into().expect("wrong size"));
+            let size = usize::from_be_bytes((&data[4..12]).try_into().expect("wrong size"));
+            let value2 = String::from_utf8_lossy(&data[12..12+size]).to_string();
+
+            Self::new(value, value2)
         }
     }
 }
@@ -22,19 +33,16 @@ pub mod test{
 
 #[cfg(test)]
 pub mod tests {
-    use crate::check_age;
+    use crate::test::TestStruct;
 
     #[test]
-    fn test_check_age_out_of_range() {
-        assert_eq!(check_age(200), false);
-        assert_eq!(check_age(201), false);
-        assert_eq!(check_age(0), false);
-        assert_eq!(check_age(-1), false);
-    }
+    fn test_teststruct() {
+        let test = TestStruct::new(1999, String::from("fdpcacacaca"));
 
-    #[test]
-    fn test_check_age_in_range() {
-        assert_eq!(check_age(1), true);
-        assert_eq!(check_age(199), true);
+        let buf = test.encode();
+
+        let test2 = TestStruct::decode(buf.as_slice());
+
+        println!("{} {}", test2.value, test2.value2);
     }
 }
